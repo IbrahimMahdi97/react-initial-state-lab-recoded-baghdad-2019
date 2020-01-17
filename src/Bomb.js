9ed95bc4b2366f9538ec43364112121b8e70cc23
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-17 10:53:13 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -12,7 +12,7 @@ class Bomb extends React.Component {
   }
   
   render() {
-    const message = this.state.secondsLeft === 0 ? 'Boom!' : 
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I`
     return (
       <div>{message}</div>
       )
